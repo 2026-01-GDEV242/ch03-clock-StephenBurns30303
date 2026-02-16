0e3f1c744255465ec275b9779a910c20f269483b
StephenBurns <123398787+StephenBurns30303@users.noreply.github.com>
2026-02-16 12:06:12 -0500

    Header Change comments
    Header is changed to better reflect the new change

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,15 +1,15 @@
 
 /**
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
+ * American-style 12 hour clock. The clock shows hours and minutes and meridian. Theinternal
  * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
- * midnight).
+ * midnight). The clock display is responsible for converting this to the US style.
  * 
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
+ * @author Stephen M. Burns
  * @version 2016.02.29
  */
 public class ClockDisplay
